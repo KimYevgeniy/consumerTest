8c2554ad00ddaad44b7db19985c0bc9bc60597a0
Yevgeniy Kim <kimeval@gmail.com>
2023-11-23 13:52:17 +0600

    fix: removed images

--- src/components/assets.jsx
+++ src/components/assets.jsx
@@ -8,12 +8,12 @@ import post4Bot from "../assets/post4.bot.jpg";
 import post4Mid from "../assets/post4.mid.jpg";
 import post5Bot from "../assets/post5.bot.jpg";
 import post5Mid from "../assets/post5.mid.jpg";
-import post6Bot from "../assets/post6.bot.jpg";
-import post6Mid from "../assets/post6.mid.jpg";
+// import post6Bot from "../assets/post6.bot.jpg";
+// import post6Mid from "../assets/post6.mid.jpg";
 import post7Bot from "../assets/post7.bot.jpg";
 import post7Mid from "../assets/post7.mid.jpg";
-import post8Bot from "../assets/post8.bot.jpg";
-import post8Mid from "../assets/post8.mid.jpg";
+// import post8Bot from "../assets/post8.bot.jpg";
+// import post8Mid from "../assets/post8.mid.jpg";
 import post9Bot from "../assets/post9.bot.jpg";
 import post9Mid from "../assets/post9.mid.jpg";
 
@@ -28,12 +28,12 @@ const assets = [
   post4Mid,
   post5Bot,
   post5Mid,
-  post6Bot,
-  post6Mid,
+  // post6Bot,
+  // post6Mid,
   post7Bot,
   post7Mid,
-  post8Bot,
-  post8Mid,
+  // post8Bot,
+  // post8Mid,
   post9Bot,
   post9Mid,
 ];
